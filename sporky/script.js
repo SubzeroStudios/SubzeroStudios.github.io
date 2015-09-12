7aad0793ee3dd37d246a64fd95c11edb7d57672b
SubzeroStudios <alex57164@yahoo.com>
2015-09-12 17:43:44 -0400

    styling to the spork page

--- sporky/script.js
+++ sporky/script.js
@@ -1,56 +1,55 @@
 $(document).ready(function(){
 	function submits(textData, lang)
 	{
-		if(!lang) translateToSpork(textData);
+		if(!lang)translateToSpork(textData);
 		else translateToEnglish(textData);
 	}
 
 	function translateToSpork(english)
 	{
-		var sporkLetters = ["z","x","c","v","b","n","m"];
-		var finalResult = [];
-		var englishWords = english.split(" ");
-
-		for(i = 0; i <= englishWords.length-1; i++)
+		var sporkLetters=["z","x","c","v","b","n","m"];
+		var finalResult=[];
+		var englishWords=english.split(" ");
+		for(i=0;i<=englishWords.length-1;i++)
 		{
 			var letters = englishWords[i].split("");
-			var sporkLetterToUse = 0;
-				for(l = 0; l <= letters.length-1; l++)
+			var sporkLetterToUse=0;
+				for(l=0;l<=letters.length-1;l++)
 				{
-					finalResult.push(letters[l]+""+sporkLetters[sporkLetterToUse]);
+					finalResult.push(letters[l]+sporkLetters[sporkLetterToUse]);
 					sporkLetterToUse++;
-					if(sporkLetterToUse > sporkLetters.length-1) sporkLetterToUse = 0;
+					if(sporkLetterToUse>sporkLetters.length-1)sporkLetterToUse=0;
 				}
 				finalResult.push(" ");
 		}
-		finalResult = finalResult.join("");
-		document.getElementById('text2').value = finalResult.toLowerCase();
+		finalResult=finalResult.join("");
+		document.getElementById('text2').value=finalResult.toLowerCase();
 	}
 
 	function translateToEnglish(spork)
 	{
-		var finalResult = [];
-		var sporkWords = spork.split(" ");
-		for(i = 0; i <= sporkWords.length-1; i++)
+		var finalResult=[];
+		var sporkWords=spork.split(" ");
+		for(i=0;i<=sporkWords.length-1;i++)
 		{
-			var letters = sporkWords[i].split("");
-			for(l = 0; l <= letters.length-1; l++)
+			var letters=sporkWords[i].split("");
+			for(l=0;l<=letters.length-1;l++)
 			{
-				if(l % 2 == 0)
+				if(l%2==0)
 				{
 					finalResult.push(letters[l]);
 				}
 			}
 			finalResult.push(" ");
 		}
-		finalResult = finalResult.join("");
-		document.getElementById('text').value = finalResult.toLowerCase();
+		finalResult=finalResult.join("");
+		document.getElementById('text').value=finalResult.toLowerCase();
 	}
 
 	$("#text").keyup(function(){
-           submits($(this).val(), 0)
+        submits($(this).val(),0)
      });
 	$("#text2").keyup(function(){
-           submits($(this).val(), 1)
+        submits($(this).val(),1)
      });
 });
