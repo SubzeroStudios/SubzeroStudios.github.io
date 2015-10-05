100d59fb228af2060889b7a3381c6e0490343827
SubzeroStudios <alex57164@yahoo.com>
2015-10-04 21:24:04 -0400

    Added menu system

--- meme/mainScript.js
+++ meme/mainScript.js
@@ -6,17 +6,22 @@ var boardDems = {
 var ty = 0;
 var tx = 0;
 var tiles = [];
-var bombDensity = 25; //x out of every 100 will be a bomb
+var tilesUncovered = 0;
+var totalTiles = 0;
+var totalBombs = 0;
 var inProg = false;
 var clockInt;
-
+var choice;
 $(document).bind("contextmenu",function(e){e.preventDefault();});
 
-function generateBoard(x, y)
+function generateBoard(x, y, bd)
 {
+	$('#menu').fadeOut(50);
+	$('#gameContainer').fadeIn(250);
+
 	clearInterval(clockInt);
 	inProg = true;
-	//console.log("generated board with dems of "+x+" , "+y);
+
 	var screenDems = {
 		width: x*tileWidth,
 		height: y*tileWidth
@@ -26,9 +31,11 @@ function generateBoard(x, y)
 	ty = 0;
 	tx = 0;
 
-	$('#gameContainer').empty(); //safety net
-	$('#gameContainer').css({"width":""+screenDems.width+"px","height":""+screenDems.height+"px"});
+	//$('#menu').css({"width":""+screenDems.width+"px","height":""+screenDems.height+"px",   "margin-top":"-"+screenDems.height+"px"});
 
+	$('#gameContainer').empty(); //safety net
+	$('#gameContainer').css({"width":""+(screenDems.width)+"px","height":""+(screenDems.height)+"px"});
+	$('#menu').css({"width":""+(900)+"px","height":""+(500)+"px", "opacity":"0.7"});
 	var serial = 1;
 
 	/* print tiles to the screen */
@@ -41,7 +48,7 @@ function generateBoard(x, y)
 		}
 
 		var b = false;
-		if(Math.floor((Math.random() * 100) + 1) <= (bombDensity))
+		if(Math.floor((Math.random() * 100) + 1) <= (bd))
 		{
 			b = true;
 		}
@@ -56,6 +63,7 @@ function generateBoard(x, y)
 		/* make the square a bomb */
 		if(t.bomb)
 		{
+			totalBombs++;
 			$('<div style="width:'+tileWidth+'px;height:'+tileWidth+'px;background-color:black;position:absolute;top:'+t.y+'px;left:'+t.x+'px;color:#f00;text-align:center;background-image:url(explosion.gif), url(emptybox2.png);"></div>').appendTo('#gameContainer');
 			$('<div id="'+serial+'" class="cover" style="width:'+tileWidth+'px;height:'+tileWidth+'px;background-color:black;position:absolute;top:'+t.y+'px;left:'+t.x+'px;color:#f00;text-align:center;background-image:url(emptybox.png);cursor:pointer;" onclick='+"showTile('bomb', '"+serial+"')"+'></div>').appendTo('#gameContainer');
 		}
@@ -134,17 +142,27 @@ function generateBoard(x, y)
 		if(e.which === 1) //left
 		{
 			tiles.forEach(function(t){
-				if(t.x == left && t.y == top && (!t.flagged))
+				if(t.x == left && t.y == top && (!t.flagged) && inProg)
 				{
-					$('#'+t.ser).hide(50);
+					$('#'+t.ser).fadeOut(50);
 					t.turned = true;
+					if(!t.bomb)
+					{
+						tilesUncovered++;
+					}
+					else //if it is a bomb
+					{
+						inProg = false;
+						 endGame(0);
+					}
+					//console.log(tilesUncovered);
 				}
 			});
+
+			if((totalTiles - totalBombs == tilesUncovered) && inProg) endGame(1);
 		}
 		if(e.which === 3) //right
 		{
-			console.log("right-click");
-			//$(this).css({"background-image":"url(flag.png)"});
 			tiles.forEach(function(t){
 				if(t.x == left && t.y == top)
 				{
@@ -182,64 +200,199 @@ function clock()
 						/* top left */
 						if(l.x-tileWidth == t2.x && l.y-tileWidth == t2.y)
 						{
-							$('#'+t2.ser).hide();
+							if(!t2.turned) tilesUncovered++;
+							$('#'+t2.ser).fadeOut(50);
 							t2.turned = true;
 						}	
 						/* top middle */
 						if(l.x == t2.x && l.y-tileWidth == t2.y)		
 						{
-							$('#'+t2.ser).hide();
+							if(!t2.turned) tilesUncovered++;
+							$('#'+t2.ser).fadeOut(50);
 							t2.turned = true;
 						}
 						/* top right */
 						if(l.x+tileWidth == t2.x && l.y-tileWidth == t2.y)	
 						{
-							$('#'+t2.ser).hide();
+							if(!t2.turned) tilesUncovered++;
+							$('#'+t2.ser).fadeOut(50);
 							t2.turned = true;
 						}
 						/* middle right */
 						if(l.x+tileWidth == t2.x && l.y == t2.y)
 						{
-							$('#'+t2.ser).hide();
+							if(!t2.turned) tilesUncovered++;
+							$('#'+t2.ser).fadeOut(50);
 							t2.turned = true;
 						}
 						/* bottom right */
 						if(l.x+tileWidth == t2.x && l.y+tileWidth == t2.y)
 						{
-							$('#'+t2.ser).hide();
+							if(!t2.turned) tilesUncovered++;
+							$('#'+t2.ser).fadeOut(50);
 							t2.turned = true;
 						}
 						/* bottom middle */
 						if(l.x == t2.x && l.y+tileWidth == t2.y)
 						{
-							$('#'+t2.ser).hide();
+							if(!t2.turned) tilesUncovered++;
+							$('#'+t2.ser).fadeOut(50);
 							t2.turned = true;
 						}
 						/* bottom left */
 						if(l.x-tileWidth == t2.x && l.y+tileWidth == t2.y)
 						{
-							$('#'+t2.ser).hide();
+							if(!t2.turned) tilesUncovered++;
+							$('#'+t2.ser).fadeOut(50);
 							t2.turned = true;
 						}
 						/* middle left */
 						if(l.x-tileWidth == t2.x && l.y == t2.y)
 						{
-							$('#'+t2.ser).hide();
+							if(!t2.turned) tilesUncovered++;
+							$('#'+t2.ser).fadeOut(50);
 							t2.turned = true;
 						}
 					});
 					l.counted = true;
+					if((totalTiles - totalBombs == tilesUncovered) && inProg) endGame(1);
 				}
 			});
-		},100);
+		},50);
 	}
 }
 
-function hi()
+function endGame(win) //win=0 is a LOSS | win=1 is a WIN
 {
-	if($('#bombcoord').val() !== null) bombDensity = $('#bombcoord').val();
-	if(bombDensity > 100) bombDensity = 100;
-	generateBoard($('#xcoord').val(),$('#ycoord').val());
+	$('#gameContainer').delay(750).fadeOut(400, function(){
+		$('#menu').fadeIn(400);
+	});
+	if(win)
+	{
+		$('#menu #menuSubCont img').attr("src", "cage.gif");
+		$('#menuMainCont').css({"margin-top":"-600px"});
+		var dif2 = "Custom";
+		switch(choice)
+		{
+			case 1:
+				dif = "Beginner";
+				break;
+
+			case 2:
+				dif = "Easy";
+				break;
+
+			case 3:
+				dif = "Medium";
+				break;
+
+			case 4:
+				dif = "Hard";
+				break;
+
+			case 5:
+				dif = "Satan";
+				break;
+		}
+		$('#menu #menuSubCont #endMessage').css({"color":"#fff"});
+		$('#menu #menuSubCont #endMessage').html("Congratulations! <br> You Cleared 100% Of The Empty Tiles On "+dif);
+	}
+	else
+	{ 
+		$('#menu #menuSubCont img').attr("src", "neil.gif");
+		$('#menuMainCont').css({"margin-top":"-600px"});
+		var dif2 = "Custom";
+		switch(choice)
+		{
+			case 1:
+				dif = "Beginner";
+				break;
+
+			case 2:
+				dif = "Easy";
+				break;
+
+			case 3:
+				dif = "Medium";
+				break;
+
+			case 4:
+				dif = "Hard";
+				break;
+
+			case 5:
+				dif = "Satan";
+				break;
+		}
+		/*
+			var tilesUncovered = 0;
+			var totalTiles = 0;
+			var totalBombs = 0;
+		*/
+
+		console.log(tilesUncovered,totalTiles,totalBombs);
+		var tilescleared = tilesUncovered/(totalTiles-totalBombs)*100;
+
+		$('#menu #menuSubCont #endMessage').css({"color":"#000"});
+		$('#menu #menuSubCont #endMessage').html("You Got Rekt m8! <br> You Only Cleared "+Math.round(tilescleared*10)/10+"% Of The Empty Tiles On "+dif);
+ 	}
+}
+
+function returnToMenu()
+{
+	$('#menuMainCont').animate({"margin-top":"0px"},500);
+}
+
+function sendData(a) //0:CUTSOM 1:very easy  2:easy  3:medium  4:hard  5.extreme
+{
+	var x, y, bd;
+	choice = a;
+	switch(a)
+	{
+		case 0: //custom
+			if(bd > 100) bd = 100;
+			x = $('#xcoord').val();
+			y = $('#ycoord').val();
+			if($('#bombcoord').val() !== null && ($('#bombcoord').val() <= 100 && $('#bombcoord').val() >= 1 )) bd = $('#bombcoord').val();
+			break;
+
+		case 1:
+			x = 10;
+			y = 10;
+			bd = 10;
+			break;
+
+		case 2:
+			x = 15;
+			y = 15;
+			bd = 15;
+			break;
+
+		case 3:
+			x = 20;
+			y = 20;
+			bd = 25;
+			break;
+
+		case 4:
+			x = 25;
+			y = 25;
+			bd = 30;
+			break;
+
+		case 5:
+			x = 50;
+			y = 50;
+			bd = 40;
+			break;
+	}
+
+	// reset variables from previous game
+	totalTiles = x * y;
+	totalBombs = 0;
+	tilesUncovered = 0; //resets just incase
+
+	generateBoard(x,y,bd);
 }
 
 
