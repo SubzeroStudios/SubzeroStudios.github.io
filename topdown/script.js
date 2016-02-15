fa157b93d4ecc10e3ba9b8a73d29f9148720cc19
SubzeroStudios <alex57164@yahoo.com>
2016-02-15 17:39:56 -0500

    added tree collision

--- topdown/script.js
+++ topdown/script.js
@@ -7,7 +7,7 @@ var screenOffsetY = 700;
 var playerPosX = screenOffsetX + ((parseInt(($('#gameWindow').css("width")).slice(0, -2))) / 2);
 var playerPosY = screenOffsetY + ((parseInt(($('#gameWindow').css("height")).slice(0, -2))) / 2);
 
-var playerSpeed = 5;
+var playerSpeed = 7.5;
 
 /* holds the keycodes for certain keys */
 var keyUp = 38;
@@ -37,18 +37,15 @@ function init()
 			type: flowerType,
 			id: i
 		});
-
-		//$("<div class='flower' style='background-image:url(assets/flower1.png);left:"+flowerXPos+"px;top:"+flowerYPos+"px'></div>").appendTo("#gamePan");
 	}
 
 	/* make some dank 420 trees and at them in their arrays */
-	var treeDensity = 275; //125
+	var treeDensity = 75; //125
 	for (var i = 0; i <= treeDensity-1; i++) 
 	{
 		var treeXPos = Math.floor((Math.random() * (parseInt(($('#gamePan').css("width")).slice(0, -2)))) + 1);
 		var treeYPos = Math.floor((Math.random() * (parseInt(($('#gamePan').css("height")).slice(0, -2)))) + 1);
 		var treeType = Math.floor(Math.random() * 2) + 1;
-
 
 		trees.push({
 			xPos: treeXPos,
@@ -73,6 +70,11 @@ function init()
 
 		if(playerPosY+100 == i) $("<div id='player' style='background-image:url(assets/playerFront.png);left:100px;top:100px;z-index:"+(playerPosY+100)+";'></div>").appendTo("#gamePan");
 	}
+
+	/* create the minimap */
+	$('<div id="miniMap"></div>').appendTo("#gameWindow");
+	$('<div id="playerIcon"></div>').appendTo("#gameWindow");
+
 }
 init();
 
@@ -100,13 +102,33 @@ function update()
 	$('#gamePan').css({"margin-left":"-"+screenOffsetX+"px",
 					   "margin-top":"-"+screenOffsetY+"px"});
 
+
+
 	if(keys[keyDown])
 	{
 		//if the player wont go off the screen
 		if(screenOffsetY <= parseInt(($('#gamePan').css("height")).slice(0, -2)) - playerSpeed - (parseInt(($('#gameWindow').css("height")).slice(0, -2))))
 		{
 			//if the player wont walk through a wall
-			if(true)
+			var canContinue = true;
+
+			trees.forEach(function(t){
+				/* if player lines up with the tree X-wise
+					50 is the width of the player
+					100 is the width of the tree */
+				if((playerPosX >= (t.xPos-50)) && (playerPosX <= (t.xPos + 100)))
+				{
+					if((playerPosY + 100) < (t.yPos + 200)) //if the player is above the tree
+					{
+						if(   ((playerPosY+playerSpeed) > (t.yPos+75)))
+						{
+							canContinue = false;
+						}
+					}
+				}
+			});
+
+			if(canContinue)
 			{
 				screenOffsetY += playerSpeed;
 				playerPosY += playerSpeed
@@ -121,8 +143,30 @@ function update()
 		//if the player wont go off the screen
 		if((screenOffsetY - playerSpeed) >= 0)
 		{
-			screenOffsetY -= playerSpeed;
-			playerPosY -= playerSpeed
+			//if the player wont walk through a wall
+			var canContinue = true;
+
+			trees.forEach(function(t){
+				/* if player lines up with the tree X-wise
+					50 is the width of the player
+					100 is the width of the tree */
+				if((playerPosX >= (t.xPos-50)) && (playerPosX <= (t.xPos + 100)))
+				{
+					if((playerPosY + 100) >= (t.yPos + 200)) //if the player is below the tree
+					{
+						if(   ((playerPosY-playerSpeed) < (t.yPos+100)))
+						{
+							canContinue = false;
+						}
+					}
+				}
+			});
+
+			if(canContinue)
+			{
+				screenOffsetY -= playerSpeed;
+				playerPosY -= playerSpeed;
+			}
 			$("#player").css({"background-image":"url(assets/playerBack.png)"});
 		}
 	}
@@ -149,8 +193,15 @@ function update()
 
 	if(keys[keyH] && !hasBuiltHouse) buildHouse();
 
+	/* actual on screen player (not dot on minimap) */
 	$('#player').css({"top":""+playerPosY+"px","left":""+playerPosX+"px","z-index":""+(playerPosY+100)+""});
 
+	/* player's icon on the minimap */
+	$('#playerIcon').css({
+		"top":""+(parseInt(($('#miniMap').css("top")).slice(0, -2))+(((playerPosY+50)/5000)*parseInt(($('#miniMap').css("height")).slice(0, -2))))+"px",
+		"left":""+(parseInt(($('#miniMap').css("left")).slice(0, -2))+(((playerPosX+25)/5000)*parseInt(($('#miniMap').css("width")).slice(0, -2))))+"px",
+		"z-index":""+(playerPosY+100)+""
+	});
 
 	/* correct the layaring of the player with the background elements MAKE MORE EFFICIENT YA FO0KW1t */
 	/*trees.forEach(function(t){
@@ -165,11 +216,17 @@ function update()
 	});*/
 }
 
+
 function buildHouse()
 {
 	/* [x, y, length, hight]  */
 	wallsArray = [
-		[screenOffsetX+500, screenOffsetY+250, 300, 15]
+		{
+			x: screenOffsetX+500,
+			y: screenOffsetY+250,
+			l: 300,
+			h: 15
+		},
 	];
 
 	houses.push({
@@ -184,7 +241,7 @@ function drawHouse()
 {
 	for(var i = 0; i <= houses[0].walls.length-1; i++)
 	{
-		$("<div class='houseWall' style='width:"+houses[0].walls[i][2]+"px; height:"+houses[0].walls[i][3]+";top:"+houses[0].walls[i][1]+";left:"+houses[0].walls[i][0]+";'></div>").appendTo("#gamePan");
+		$("<div class='houseWall' style='width:"+houses[0].walls[i].l+"px; height:"+houses[0].walls[i].h+";top:"+houses[0].walls[i].y+";left:"+houses[0].walls[i].x+";'></div>").appendTo("#gamePan");
 	}
 
 	hasBuiltHouse = true;
